1c747587e96fca36bce4f57d467ce6ff797b3e44
Beau Elliott <belliott1516@gmail.com>
2022-06-15 15:17:41 -0700

    completed fourteenth test

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,6 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT category.name, AVG(film.length) AS avg FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name 
