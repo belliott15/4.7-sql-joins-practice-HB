8e5ac9a5bccd5ad71960b1df401c3d384fa5a649
Beau Elliott <belliott1516@gmail.com>
2022-06-15 14:50:01 -0700

    got eleventh test to pass

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,9 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT film.title, COUNT(film.film_id) AS count FROM inventory
+LEFT JOIN film ON inventory.film_id = film.film_id
+LEFT JOIN rental ON rental.inventory_id = inventory.inventory_id
+GROUP BY film.film_id
+ORDER BY count DESC
+LIMIT 10
